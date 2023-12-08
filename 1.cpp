29375a3e1dea44091fac50b3ebf830f9283084ee
Kandala Chetan Kumar <chetankandala2017@gmail.com>
2023-12-08 15:15:18 +0530

    chuup

---
+++ 1.cpp
@@ -0,0 +1,189 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+// ----------------------LPS----------------------------
+void LPS(string pat, int M, vector<int>& lps)
+{
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else 
+        {
+            if (len != 0) {
+                len = lps[len - 1];
+            }
+            else 
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+// ----------------------GCD----------------------------
+
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+// ----------------------DSU----------------------------
+
+
+class DSU { 
+    int* parent; 
+    int* rank; 
+  
+public: 
+    DSU(int n) 
+    { 
+        parent = new int[n]; 
+        rank = new int[n]; 
+  
+        for (int i = 0; i < n; i++) { 
+            parent[i] = -1; 
+            rank[i] = 1; 
+        } 
+    } 
+  
+    // Find function 
+    int find(int i) 
+    { 
+        if (parent[i] == -1) 
+            return i; 
+  
+        return parent[i] = find(parent[i]); 
+    } 
+  
+    // Union function 
+    void unite(int x, int y) 
+    { 
+        int s1 = find(x); 
+        int s2 = find(y); 
+  
+        if (s1 != s2) { 
+            if (rank[s1] < rank[s2]) { 
+                parent[s1] = s2; 
+            } 
+            else if (rank[s1] > rank[s2]) { 
+                parent[s2] = s1; 
+            } 
+            else { 
+                parent[s2] = s1; 
+                rank[s1] += 1; 
+            } 
+        } 
+    } 
+}; 
+//------------------------------------------------------------------------------------------------------
+
+string minLexRotation(string str)
+{
+    // Find length of given string
+    long long n = str.length();
+ 
+    // Create an array of strings to store all rotations
+    string arr[n];
+ 
+    // Create a concatenation of string with itself
+    string concat = str + str;
+ 
+    // One by one store all rotations of str in array.
+    // A rotation is obtained by getting a substring of concat
+    for (long long i = 0; i < n; i++)
+        arr[i] = concat.substr(i, n);
+ 
+    // Sort all rotations
+    sort(arr, arr+n);
+ 
+    // Return the first rotation from the sorted array
+    return arr[0];
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("Error.txt", "w", stderr);
+#endif
+//======================================================================================================
+// write your code here
+
+	long long n, m, curr, ticket;
+    cin >> n >> m;
+    multiset<long long, greater<int>> tickets;
+
+    while (n--)
+    {
+        cin >> ticket;
+        tickets.insert(ticket);
+    }
+
+    while (m--)
+    {
+        cin >> curr;
+        auto it = tickets.lower_bound(curr);
+        if (it == tickets.end())
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << *it << endl;
+            tickets.erase(it);
+        }
+    }
+    return 0;
+}
